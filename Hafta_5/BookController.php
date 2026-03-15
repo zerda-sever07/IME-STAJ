45dbbeca6070effcd2185cc17db5516412bc6742
Zerda Sever <zerda1343@gmail.com>
2026-03-15 23:15:39 +0300

    Add files via upload

---
+++ Hafta_5/BookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::paginate(5);
+
+        return response()->json($books);
+    }
+}
